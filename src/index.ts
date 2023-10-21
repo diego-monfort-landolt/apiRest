e64727de3564f399d35b9ba0cdfd60c75afd9164
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-10-21 15:51:11 +0200

    added src and services

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express'
+import diaryRouter from './routes/diaries'
+
 const app = express()
 
 app.use(express.json()) // middleware que transforma req body a un json
-const PORT = 3000;
-app.get('/ping', (req, res) => {
-  console.log('ping Ping ping')
+
+const PORT = 3000
+
+app.get('/ping', (_req, res) => {
+  
+  console.log('ping Ping ping' + new Date().toLocaleDateString())
   res.send('pong')
 })
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`)
